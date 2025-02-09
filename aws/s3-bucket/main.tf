1818bb23416473b885acc3b30892a33eb333f7f0
Patrick Hobusch <patrick@hobusch.net>
2025-02-09 14:02:12 +0800

    Prevent public access, allow confige versioning, improve IP filtering

--- aws/s3-bucket/main.tf
+++ aws/s3-bucket/main.tf
@@ -13,30 +13,58 @@ resource "aws_s3_bucket" "s3_bucket" {
   object_lock_enabled = var.enable_object_lock
 }
 
+resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
+  bucket                  = aws_s3_bucket.s3_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
+  count = var.enable_versioning ? 1 : 0
+
+  bucket = aws_s3_bucket.s3_bucket.id
+  versioning_configuration { status = "Enabled" }
+}
+
 data "aws_iam_policy_document" "iam_policy_document_s3_dedicated_access" {
   version = "2012-10-17"
+
   statement {
     effect = "Allow"
+
     principals {
       type        = "AWS"
       identifiers = ["${aws_iam_user.iam_user.arn}"]
     }
-    actions = [
-      "s3:PutObject",
-      "s3:GetObject",
-      "s3:GetObjectAttributes",
-      "s3:ListBucket",
-      "s3:GetBucketVersioning",
-      "s3:DeleteObject"
-    ]
+
+    actions = concat(
+      [
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:GetObjectAttributes",
+        "s3:ListBucket",
+        "s3:DeleteObject"
+      ],
+      var.enable_object_lock || var.enable_versioning ? [
+        "s3:GetBucketVersioning"
+      ] : []
+    )
+
     resources = [
       "${aws_s3_bucket.s3_bucket.arn}",
       "${aws_s3_bucket.s3_bucket.arn}/*",
     ]
-    condition {
-      test     = "IpAddress"
-      variable = "aws:SourceIp"
-      values   = var.allowed_ips
+
+    dynamic "condition" {
+      for_each = length(var.allowed_ips) > 0 ? [1] : []
+
+      content {
+        test     = "IpAddress"
+        variable = "aws:SourceIp"
+        values   = var.allowed_ips
+      }
     }
   }
 }
--- aws/s3-bucket/variables.tf
+++ aws/s3-bucket/variables.tf
@@ -1,20 +1,30 @@
 variable "name" {
-  type        = string
   description = "Name of the bucket (without prefix)"
+  type        = string
+  validation {
+    condition     = can(regex("^[a-z0-9.-]+$", var.name))
+    error_message = "Invalid S3 bucket name."
+  }
 }
 
 variable "prefix" {
-  type        = string
   description = "Prefix of the bucket (to make it globally unique)"
+  type        = string
 }
 
 variable "enable_object_lock" {
+  description = "Enable object lock (implies enabling versioning)"
   type        = bool
-  description = "Enable object lock"
+  default     = false
+}
+
+variable "enable_versioning" {
+  description = "Enable versioning"
+  type        = bool
   default     = false
 }
 
 variable "allowed_ips" {
-  type        = list(string)
   description = "List of allowed IPs"
+  type        = list(string)
 }
